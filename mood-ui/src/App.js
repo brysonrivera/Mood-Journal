2f67c053180af983838b97cff316ae51960c7a5d
Bryson Rivera <brysonm.rivera@gmail.com>
2022-04-24 18:32:50 -0700

    features added: basic UI includes buttons and a text area
    Basic implementation of the UI for the home page has been added. User can not enter in text in the box and submit. Submit button does not work, as it still requires me to implement a backend which is the next step from here.

--- mood-ui/src/App.js
+++ mood-ui/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [entry, setEntry] = useState('');
+
+
+  const newEntry = () => {
+    console.log(entry)
+    // now I need to start working on implementing the api. 
+  }
+
+
+
   return (
     <div>
       <h1>Mood Journal</h1>
+      <textarea
+        className="Journal-Entry"
+        value={entry}
+        type="text"
+        onChange={(e) => setEntry(e.target.value)}
+      />
+      <div className="Testing"></div>
+      <div>
+        <button onClick={() => newEntry()}>Submit</button>
+      </div>
+
+
     </div>
   );
 }
